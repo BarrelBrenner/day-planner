2400de653aba18f0603691f44799aa4126d33a11
BarrelBrenner <rondolph@aol.com>
2021-05-06 21:08:16 -0500

    Day Planner Final Draft

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,35 @@
-//List of let statements
+//List of let statements.
 let workDayScheduler = [];
 let currentDate = getCurrentDate();
-let saveIcon = $("<i class='far fa-save fa-lg'></i>");
-let saveEnd = $("<button>").addClass("col-md-1 saveBtn");
 
-//This function shows the current date & time of last refresh
+//This function shows the current date & time of last refresh.
 function getCurrentDate() {
   let currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
   $("#currentDay").text(currentDate);
 }
 
-//Function for determination of time & an if/else statement to signify the current time
+//This function determines time, let statements categorize, & an if/else statements to signify current time.
 for (time = 00; time <= 23; time++) {
   let now = time - 00;
   let yourTask = "";
   let currentHour = 0;
   let midDay = "";
-
+  
   if (time === 12) {
     currentHour = 12;
     midDay = "pm";
+  
   } else if (time > 12) {
     currentHour = time - 12;
     midDay = "pm";
+  
   } else if (time < 12) {
     currentHour = time;
     midDay = "am";
   }
   currentHour = currentHour.toString();
-
-  //List of how current tasks are categorized & broken down in parts
+  
+//List of how current tasks are categorized & broken down in parts.
   yourTask = {
     now: now,
     currentHour: currentHour,
@@ -40,17 +40,17 @@ for (time = 00; time <= 23; time++) {
   workDayScheduler.push(yourTask);
 }
 
-//Functions to store & display data into/from Local Storage.
+//These functions stores & display data into/from Local Storage.
 function storeTaskData() {
   localStorage.setItem("currentDate", JSON.stringify(workDayScheduler));
 }
 function taskDataShow() {
   workDayScheduler.forEach(function (hour) {
-    $("#currentDay" + hour.now).val(hour.yourTask);
+    $(`#${hour.now}`).val(hour.yourTask);
   });
 }
 
-//Function to load data in question
+//This function loads data in question.
 function dataSetLoad() {
   let dataSetLoad = JSON.parse(localStorage.getItem("currentDate"));
   if (dataSetLoad) {
@@ -60,53 +60,58 @@ function dataSetLoad() {
   taskDataShow();
 }
 
-//List of how scheduler handles and displays input
+//List of let statements shows how scheduler handles and displays input.
 workDayScheduler.forEach(function (hour) {
   let textRow = $("<form>");
   textRow.addClass("row");
   $(".container").append(textRow);
-
+  
   let textField = $("<div>");
   textField.addClass("col-md-2 hour");
   textField.text(hour.currentHour + hour.midDay);
-
+  
   let haveInput = $("<div>");
   haveInput.addClass("col-md-9 description p-0");
-
+  
   let haveData = $("<textarea>");
+  haveData.attr("id", hour.now);
   haveData.attr("now", hour.now);
 
-//If/else statement that color codes current timestamp.
+//These If/else statements color codes current timestamp.
   if (hour.time == moment().format("HH")) {
     haveData.addClass("today");
+  
   } else if (hour.time < moment().format("HH")) {
     haveData.addClass("history");
+  
   } else if (hour.time > moment().format("HH")) {
     haveData.addClass("mystery");
   }
   haveInput.append(haveData);
-
-// This set of let commands create save button at end of row
+  
+//This set of let commands create save button at end of row.
   let saveIcon = $("<i class='far fa-save fa-lg'></i>");
   let saveEnd = $("<button>").addClass("col-md-1 saveBtn");
 
-//This set of text append elements to row
+//This set of text append elements to row.
   saveEnd.append(saveIcon);
   textRow.append(textField, haveInput, saveEnd);
 });
 
-//save button functions appropriately
+//Save button process to confirm functionality.
 $(".saveBtn").on("click", function (event) {
- event.preventDefault();
-  //saving information into array
+  event.preventDefault();
+
+//This let statement saves information in question into array.
   let saveIndex = $(this).siblings(".description").children().attr("now");
   workDayScheduler[saveIndex].yourTask = $(this).siblings(".description").children().val();
-
+  
   storeTaskData();
   taskDataShow();
 });
 
-//get current date on page load
+//This command gets current date on page load/refresh.
 getCurrentDate();
-//load data for page load
+
+//This command loads data on page load/refresh.
 dataSetLoad();
